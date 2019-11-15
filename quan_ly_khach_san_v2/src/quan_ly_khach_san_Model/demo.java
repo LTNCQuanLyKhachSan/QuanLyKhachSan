81c35f80fa076bf32971de9473645f7a1b3a905c
= <=>
2019-11-15 23:06:25 +0700

    submit

---
+++ quan_ly_khach_san_v2/src/quan_ly_khach_san_Model/demo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quan_ly_khach_san_Model;
+
+/**
+ *
+ * @author thaibinh
+ */
+public class demo {
+    
+}
